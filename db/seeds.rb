79114803b26a205d752d02e5d17073108e8067d0
Ruurd Pels <rfpels@gmail.com>
2015-05-30 11:09:25 +0200

    Add a regular user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: 'admin@mctiny.local', password: 'mysterious', admin: true)
+User.create(email: 'user@mctiny.local', password: 'mysterious', admin: false)
